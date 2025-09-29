5f29d552cbabd1c1cd8a110aa2dd5e9a71072cb5
Quentin Ågren <quentin.agren@gmail.com>
2025-09-29 18:11:37 +0200

    Add quick helper to setup QVM_DIR environment variable

---
+++ scripts/env.sh
@@ -0,0 +1,8 @@
+root_dir=/
+# Adjust depending on your setup
+qvm_dir=var/vms
+# Toolbox container
+if [[ -f /run/.containerenv ]] && [[ -d /run/host ]]; then
+  root_dir=/run/host
+fi
+export QVM_DIR="${root_dir}/${qvm_dir}"
