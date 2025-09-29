89b99c367f1ee3ae95d5ec0a4937614feee0f5f1
Quentin Ågren <quentin.agren@gmail.com>
2025-09-29 19:18:15 +0200

    Introduce a shorthand argument to select SSH target

--- scripts/qvm-ssh.sh
+++ scripts/qvm-ssh.sh
@@ -3,7 +3,7 @@ qvm_dir="${QVM_DIR}"
 ssh_private_key="${qvm_dir}/ssh/id_ed25519"
 ssh_config="${qvm_dir}/ssh/config"
 user="guest"
-instance="${1}"
+instance="${1:-0}"
 
 export TERM=xterm-256color
-ssh -q -i "${ssh_private_key}" -F "${ssh_config}" "${user}"@localhost -p 220"${instance}"
+ssh -q -i "${ssh_private_key}" -F "${ssh_config}" "${user}"@127.0.0.1 -p 220"${instance}"
