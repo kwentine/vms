773565df8a8de6e7d83c1221677e38c4666af758
Quentin Ågren <quentin.agren@gmail.com>
2025-10-02 17:08:33 +0200

    Fix env script

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,9 +1,8 @@
-root_dir=/
 # Adjust depending on your setup
 qvm_dir=var/vms
 # Toolbox container
 if [[ -f /run/.containerenv ]] && [[ -d /run/host ]]; then
   root_dir=/run/host
 fi
-export QVM_DIR="${root_dir}/${qvm_dir}"
+export QVM_DIR="${root_dir:-}/${qvm_dir}"
 export PATH="$(realpath ${BASH_SOURCE[0]%/*}):${PATH}"
