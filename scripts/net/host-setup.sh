17b02f307a10147a88b418d4aa486864992599a8
Quentin Ågren <quentin.agren@gmail.com>
2025-10-04 16:15:50 +0200

    Improve host net setup script

--- scripts/net/host-setup.sh
+++ scripts/net/host-setup.sh
@@ -1,20 +1,28 @@
-# enable IPv4 forwarding until reboot (safe, easy to revert)
-set -eux
-
-# Assumes the following permanent setup:
-# sudo firewall-cmd --new-policy qvm-nat --permanent
-# sudo firewall-cmd --reload
+#!/bin/sh
+set -eu
 
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo firewall-cmd --permanent --zone=public --add-masquerade
-sudo firewall-cmd --policy qvm-nat --add-ingress-zone internal
-sudo firewall-cmd --policy qvm-nat --add-egress-zone public
+
+policy_name="qvm-nat"
+if ! sudo firewall-cmd --info-policy="${policy_name}" &> /dev/null; then
+  echo "Creating policy ${policy_name}"
+  sudo firewall-cmd --permanent --new-policy="${policy_name}"
+  sudo firewall-cmd --permanent --policy="${policy_name}" --set-target=ACCEPT
+  sudo firewall-cmd --permanent --policy="${policy_name}" --set-short="Forward and NAT traffic from internal to public"
+  sudo firewall-cmd --reload
+fi
+
+{
+  sudo firewall-cmd --policy "${policy_name}" --add-ingress-zone internal
+  sudo firewall-cmd --policy "${policy_name}" --add-egress-zone public
+  sudo firewall-cmd --policy="${policy_name}" --add-masquerade
+  sudo firewall-cmd --info-policy="${policy_name}"
+} &> /dev/null
 
 # Add interface to the internal zone
 ifname="${1:-tap0}"
 if ! ip link show "${ifname}" &> /dev/null; then
-  echo "Interface ${ifname} must be created before it can be added to the internal zone."
-  exit 1
+  echo "Interface ${ifname} does not exist."
+else
+  sudo firewall-cmd --zone=internal --add-interface="${ifname}"
 fi
-
-sudo firewall-cmd --zone=internal --add-interface=tap0
