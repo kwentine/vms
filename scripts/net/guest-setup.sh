9233a1ce601fc93a761ea7eb1386e42c2c86145f
Quentin Ågren <quentin.agren@gmail.com>
2025-10-05 14:34:54 +0200

    Polish guest setup scritp

--- scripts/net/guest-setup.sh
+++ scripts/net/guest-setup.sh
@@ -1,8 +1,15 @@
 # set static IP, gateway via host tap, and public DNS
-sudo nmcli con mod ens3 \
-  ipv4.method manual ipv4.addresses 10.42.0.10/24 \
-  ipv4.gateway 10.42.0.1 \
-  ipv4.dns 1.1.1.1
 
-sudo nmcli con down ens3
-sudo nmcli con up ens3
+conn_settings=(
+     ipv4.method manual
+     ipv4.addresses 10.42.0.10/24
+     ipv4.gateway 10.42.0.1
+     ipv4.dns 1.1.1.1
+)
+
+sudo nmcli con add type ethernet \
+     ifname ens3 \
+     con-name ens3-static \
+     "${conn_settings[@]}"
+
+sudo nmcli con up ens3-static
