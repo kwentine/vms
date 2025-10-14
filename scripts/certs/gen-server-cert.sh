cff851c63bcee827b39214cfdab2f6bf7ba3d0de
Quentin Ågren <quentin.agren@gmail.com>
2025-10-14 10:06:29 +0200

    Reorganize cert scripts

--- scripts/certs/gen-server-cert.sh
+++ scripts/certs/gen-server-cert.sh
@@ -1,12 +1,12 @@
 args=(
   -batch # Non-interactive mode
   -x509  # Output a certificate directly, rather than a request
-  -out "${QVM_DIR}/certs/hosts/cp/cert.pem" # The generated certificate file
+  -out "${QVM_DIR}/certs/hosts/cp/server.pem" # The generated certificate file
   -newkey rsa:2048 # Generate the certified keypair
   -keyout "${QVM_DIR}/certs/hosts/cp/priv.pem" # Generated private key
   -noenc # Do not encrypt the private key
   -subj "/CN=cp.qvm0.lan" # The identity bound by the certificate to the keypair
-  -addext "subjectAltName = DNS:cp.qvm0" # Some flexibility for the valid domain names
+  -addext "subjectAltName = DNS:cp.qvm0, DNS:cp.qvm0.lan" # Some flexibility for the valid domain names
   -addext "extendedKeyUsage=critical,serverAuth"
   -addext "basicConstraints = critical, CA:false"
   -days 365
---
+++ scripts/certs/install-server-cert.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+guest="${1:-cp}"
+cert_dir=/root/cert
+# Ensure target directory exists
+
+ssh -F "${QVM_DIR}/ssh/config" "root@${guest}.qvm0.lan" mkdir -p "${cert_dir}"
+
+args=(
+  -F "${QVM_DIR}/ssh/config"
+  # Sources
+  "${QVM_DIR}/certs/hosts/${guest}/"{server,priv}.pem
+  # Dest
+  "root@${guest}.qvm0.lan:${cert_dir}"
+)
+
+scp "${args[@]}"
