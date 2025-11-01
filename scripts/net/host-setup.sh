c24c0b27ac59961d5e9f51e8150aabfbe8dd8888
Quentin Ågren <quentin.agren@gmail.com>
2025-11-01 19:02:57 +0100

    Configure host DNS

--- scripts/net/host-setup.sh
+++ scripts/net/host-setup.sh
@@ -6,3 +6,8 @@ if sudo nft -f "$(dirname $0)/qvm.nft"; then
 fi
 
 sudo sysctl -w net.ipv4.ip_forward=1
+
+# DNS resolution
+sudo resolvectl dns qvm0 10.42.0.1
+sudo resolvectl domain qvm0 "~qvm0.lan"
+sudo resolvectl default-route qvm0 no
