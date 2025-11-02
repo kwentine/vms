25fcd0429a97cc64405a3ac61f7d35862b21e67a
Quentin Ågren <quentin.agren@gmail.com>
2025-11-02 15:03:44 +0100

    Refactor qvm-dns and qvm-bridge scripts

---
+++ scripts/libqvm.sh
@@ -0,0 +1,13 @@
+dry_run=0
+
+usage() {
+  local ret=0
+  [[ "${1:-}" == -h ]] || { ret=1; exec >&2; }
+  sed -n "/^#+/{s/^#+ \?//; s/SCRIPT/${SCRIPT}/; p}" "$0"
+  exit "${ret}"
+}
+
+run() {
+  ((dry_run)) && { echo "$@"; return; }
+  "$@"
+}
