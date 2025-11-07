27f57178be74cf0266148e8631ede86f1241a274
Quentin Ågren <quentin.agren@gmail.com>
2025-11-07 16:46:59 +0100

    Correct packet capture setup

--- scripts/libqvm.sh
+++ scripts/libqvm.sh
@@ -1,10 +1,19 @@
-dry_run=0
+declare -i dry_run=0
 
 usage() {
   local ret=0
   [[ "${1:-}" == -h ]] || { ret=1; exec >&2; }
   sed -n "/^#+/{s/^#+ \?//; s/SCRIPT/${SCRIPT}/; p}" "$0"
   exit "${ret}"
+}
+
+info() {
+  echo "INFO: $*" >&2
+}
+
+errexit() {
+  echo "ERROR: $*" >&2
+  exit 1
 }
 
 run() {
--- scripts/net/pcap.sh
+++ scripts/net/pcap.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
 if [[ "${1:-dhcp}" == dhcp ]]; then
-  sudo tcpdump -v -v -v -nn -i qvm0  '(net 10.42.0.0/24 or host 255.255.255.255) and (udp dst port 67 or udp dst port 68)'
+  sudo tcpdump -nn -i qvm0 -e '(net 10.42.0.0/24 or ip broadcast) and port 67 or 68'
 fi
